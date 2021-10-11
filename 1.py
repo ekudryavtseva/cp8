c0ff7c27b09e5bad70adaf92f106600314163280
ekudryavtseva <91140274+ekudryavtseva@users.noreply.github.com>
2021-10-11 22:44:40 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,19 @@
-chislo=int(input("Введите число:"))
-ss=int(input("Введите систему счисления:"))
-if not(ss!=2) or (ss!=8):
-   quit()
-   chislo2=''
-while chislo>0:
-    chislo2=str(chislo%ss)+chislo2
-    chislo//=ss
-print(chislo2)
-   
+def perevod(a, b):
+    chislo = ''
+    while a > 0:
+        chislo = str(a % b) + chislo
+        a = a//b
+    return int(chislo)
+
+
+try:
+    print('Введите свое число')
+    numb = int(input())
+    print('Выберете 2-ичную или 8-ичную')
+    sist = int(input())
+    while sist != 8 and sist != 2:
+        print('2-ичную или 8-ичную')
+        sist = int(input())
+    print(perevod(numb, sist))
+except ValueError:
+    print('Это не число. Введите число.')
